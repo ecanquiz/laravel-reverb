f3928f8f26cc82b146e7975922543ab8b6595223
Ernesto Canquiz <caribestic123@gmail.com>
2025-03-02 17:54:57 -0400

    Done course

---
+++ routes/channels.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('messages', function ($user) {
+    return !empty($user);
+});
+
+/*Broadcast::channel('messages.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});*/
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Events\HelloWorld;
+use App\Events\MessageReceived;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
+
+
 Route::get('/', function () {
+    HelloWorld::dispatch();
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -22,6 +28,24 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::post("/messages", function () {
+    $payload = request()->validate([
+        'message' => 'required',
+        'id' => 'required'
+    ]);
+    MessageReceived::dispatch($payload['message'], $payload['id']);
+    return response()->json(['message' => 'message received']);
+});
+
+Route::get('visit-count', function () {
+    HelloWorld::dispatch();
+    return Inertia::render('VisitCount');
+    //return Inertia::render('VisitCount', [
+    //    "user" => auth()->user()
+    //]);
+
 });
 
 require __DIR__.'/auth.php';
